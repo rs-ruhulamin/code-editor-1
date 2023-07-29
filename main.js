f48caff58f59f050f6eeca1c640667b6b2d344c2
tanvinnewoussrite <140910726+tanvinnewoussrite@users.noreply.github.com>
2023-07-29 23:12:42 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function compile() {
+
+	var html = document.getElementById("html");
+	var code = document.getElementById("code").contentWindow.document;
+	
+	 document.body.onkeyup = function(){
+	    code.open();
+		code.writeln(html.value);
+		code.close();
+      };
+    };
+
+compile();
